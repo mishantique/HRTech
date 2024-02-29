72f17ba6f313a98fbe8345bf3216001a5bd6de64
mishantique <michvitko@mail.ru>
2024-02-29 19:24:22 +0300

    sorted_similarities

--- text_processing/sort_similiarity.py
+++ text_processing/sort_similiarity.py
@@ -1,19 +1,23 @@
 import json
 
-def sort_similarities_by_score(input_file, output_file):
+def sort_and_filter_similarities(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
-    # Сортировка данных по убыванию сходства
-    sorted_data = sorted(data, key=lambda x: x['similarity'][0][0], reverse=True)
+    # Сортировка данных по убыванию сходства и фильтрация нужных полей
+    sorted_and_filtered_data = sorted(
+        [{"resume_uuid": item['resume_uuid'], "similarity": item['similarity'][0][0]} for item in data], 
+        key=lambda x: x['similarity'], 
+        reverse=True
+    )
 
-    # Сохранение отсортированных данных в новый файл
+    # Сохранение отсортированных и отфильтрованных данных в новый файл
     with open(output_file, 'w', encoding='utf-8') as out_file:
-        json.dump(sorted_data, out_file, ensure_ascii=False, indent=4)
+        json.dump(sorted_and_filtered_data, out_file, ensure_ascii=False, indent=4)
 
 # Путь к файлу с данными и к выходному файлу
-input_file = 'TGGrXbXiuJZyiKyUVNnTujSAnABq9UmHAx'
-output_file = '/mnt/data/sorted_similarities.json'
+input_file = 'text_processing/similaritites.json'
+output_file = 'text_processing/sorted_similarities1.json'
 
 # Вызов функции сортировки
-sort_similarities_by_score(input_file, output_file)
+sort_and_filter_similarities(input_file, output_file)
